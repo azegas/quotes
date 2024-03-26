8f4f79f56c0c23bada5eafce3439778e121a0cb5
Arvydas <azegaspa@gmail.com>
2024-03-26 22:06:01 +0200

    implement list views for both author and quote

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from app.views import Index         
+from app.views import Index, QuoteListView, AuthorListView
 
 urlpatterns = [
-    path("", Index.as_view()),
+    path("", Index.as_view(), name="index"),
+    path("quote-list", QuoteListView.as_view(), name="quote-list"),
+    path("author-list", AuthorListView.as_view(), name="author-list"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render, HttpResponse
 from django.views import View
+from django.views.generic.list import ListView
+from app.models import Quote, Author
 
 
 
 class Index(View):
     def get(self, request):
-        print("get")
-        return HttpResponse("Welcome to quotes app")
+        message = "ROFL"
+        return render(request,"index.html", {"message" : message})
 
     # make sure to comment out the csfr middleware for this to work
     def post(self, request):
         print("posted")
         return HttpResponse("someone has posted")
+    
+
+class QuoteListView(ListView):
+    model = Quote
+
+class AuthorListView(ListView):
+    model = Author
