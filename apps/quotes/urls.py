330f13649ef322d2d7b56a1ae56970e64613fe14
Arvydas <azegaspa@gmail.com>
2024-03-29 11:55:24 +0200

    refactor url order

--- apps/quotes/urls.py
+++ apps/quotes/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("list", QuoteListView.as_view(), name="quote-list"),
     path("detail/<int:pk>", QuoteDetailView.as_view(), name="quote-detail"),
     path("create/", QuoteCreateView.as_view(), name="quote-create"),
-    path("<int:pk>/delete", QuoteDeleteView.as_view(), name="quote-delete"),
-    path("<int:pk>/update", QuoteUpdateView.as_view(), name="quote-update"),
+    path("detail/<int:pk>/delete", QuoteDeleteView.as_view(), name="quote-delete"),
+    path("detail/<int:pk>/update", QuoteUpdateView.as_view(), name="quote-update"),
 ]
