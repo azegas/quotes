8f66457d5d2d220b067dd10cbc1724d312296f0e
Arvydas <azegaspa@gmail.com>
2024-04-17 22:01:25 +0300

    tests for update view

--- tests/test_views.py
+++ tests/test_views.py
@@ -185,3 +185,140 @@ class TestViews(TestCase):
         # Check that the form contains errors
         form = response.context["form"]
         self.assertTrue(form.errors)
+
+    def test_quote_delete_view_get(self):
+        """test quote delete view, GET request"""
+
+        url = reverse("quote-delete", kwargs={"pk": self.quote1.pk})
+        response = self.client.get(url)
+
+        # Check that the response status code is 200
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the correct template is used
+        self.assertTemplateUsed(response, "quotes/quote_confirm_delete.html")
+
+    def test_quote_delete_view_post(self):
+        """test quote delete view, POST request"""
+
+        # Get the initial count of quotes
+        initial_count = Quote.objects.count()
+
+        # Get the URL for deleting the quote
+        url = reverse("quote-delete", kwargs={"pk": self.quote1.pk})
+
+        # Make a POST request to delete the quote
+        response = self.client.post(url)
+
+        # Check that the quote has been deleted
+        self.assertFalse(Quote.objects.filter(pk=self.quote1.pk).exists())
+
+        # Optionally, check the redirect after deletion
+        self.assertRedirects(response, reverse("quote-list"))
+
+        # Check that the count of quotes has decreased by 1
+        self.assertEqual(Quote.objects.count(), initial_count - 1)
+
+    def test_quote_update_view(self):
+        """test quote update view"""
+
+        url = reverse("quote-update", kwargs={"pk": self.quote1.pk})
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "quotes/quote_form.html")
+
+        # Check that the form is passed to the template context
+        self.assertTrue("form" in response.context)
+        form = response.context["form"]
+        self.assertIsInstance(form, QuoteForm)
+
+        # Check the initial form data
+        self.assertEqual(form["text"].value(), self.quote1.text)
+
+    def test_quote_update_view_get(self):
+        """test quote update view, GET request"""
+
+        # Get the URL for updating the quote
+        url = reverse("quote-update", kwargs={"pk": self.quote1.pk})
+
+        # Make a GET request to retrieve the update form
+        response = self.client.get(url)
+
+        # Check that the response status code is 200
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the correct template is used
+        self.assertTemplateUsed(response, "quotes/quote_form.html")
+
+        # Check that the form is passed to the template context
+        self.assertTrue("form" in response.context)
+        self.assertIsInstance(response.context["form"], QuoteForm)
+
+    def test_quote_update_view_post(self):
+        """test quote update view, POST request"""
+
+        # Define the form data for updating the quote
+        form_data = {
+            "text": "Updated text",
+            "author": self.author.pk,
+        }
+
+        # Get the URL for updating the quote
+        url = reverse("quote-update", kwargs={"pk": self.quote1.pk})
+
+        # Make a POST request to update the quote
+        response = self.client.post(url, data=form_data)
+
+        # Check that the response is a redirect to the quote list
+        self.assertEqual(response.status_code, 302)  # status code for redirect
+        self.assertRedirects(response, reverse("quote-list"))
+
+        # Check that the quote has been updated in the database
+        updated_quote = Quote.objects.get(pk=self.quote1.pk)
+        self.assertEqual(updated_quote.text, form_data["text"])
+        self.assertEqual(updated_quote.author.pk, form_data["author"])
+
+    def test_quote_update_view_post_invalid(self):
+        """Test quote update view with invalid form data, POST request"""
+
+        # Define invalid form data with empty text field
+        form_data = {
+            "text": "",  # Empty text field
+            "author": self.author.pk,
+        }
+
+        # Get the URL for updating the quote
+        url = reverse("quote-update", kwargs={"pk": self.quote1.pk})
+
+        # Make a POST request with invalid form data
+        response = self.client.post(url, data=form_data)
+
+        # Check that the response status code is 200
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the correct template is used
+        self.assertTemplateUsed(response, "quotes/quote_form.html")
+
+        # Check that the form is passed to the template context
+        self.assertTrue("form" in response.context)
+        form = response.context["form"]
+        self.assertIsInstance(form, QuoteForm)
+
+        # Check that the form is bound and contains errors
+        self.assertTrue(form.is_bound)
+        self.assertTrue(form.errors)
+
+        # Optionally, you can check specific error messages if needed
+        self.assertTrue("text" in form.errors)
+        self.assertEqual(form.errors["text"], ["This field is required."])
+
+        # Verify that the quote object is not updated in the database
+        self.assertNotEqual(
+            Quote.objects.get(pk=self.quote1.pk).text, form_data["text"]
+        )
+
+        # Verify that the quote object is same as at the beginning
+        self.assertEqual(
+            Quote.objects.get(pk=self.quote1.pk).text, "Test Quote 1"
+        )
