e685465496b21d7f3f2911676b09fb6fd13a7b81
nixos <azegaspa@gmail.com>
2024-06-07 04:54:43 +0300

    add docstrings

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,9 +1,12 @@
+"""Views for dashboard app"""
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
 @login_required
 def profile(request):
+    """Mandatory profile page for allauth."""
 
     # Get the logged-in user
     user = request.user
